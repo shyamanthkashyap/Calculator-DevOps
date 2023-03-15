5ccb907d18aa090788396e4e52c23a932b37ced9
Shyamanth Kashyap <shyamanth.giggs@gmail.com>
2023-03-15 12:30:36 +0530

    Initial Commit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,6 @@
+package org.example;
+import java.util.Scanner;
+
+public class Calculator {
+
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,6 @@
+import org.example.Calculator;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+}
