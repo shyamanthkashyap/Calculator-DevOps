ae86600afb6b53e634bab98df90725a0af6e6074
Shyamanth Kashyap <shyamanth.giggs@gmail.com>
2023-03-15 14:53:03 +0530

    Calculator Functionality Commit

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,5 +2,64 @@ package org.example;
 import java.util.Scanner;
 
 public class Calculator {
-
+    public static double factorial(double num) {
+        double factorial = 1;
+        for(int i = 1; i <= num; i++){
+            factorial *= i;
+        }
+        return factorial;
+    }
+    public static double squareRoot(double num){
+        return Math.sqrt(num);
+    }
+    public static double power(double num1, double num2){
+        return Math.pow(num1,num2);
+    }
+    public static double naturalLog(double num){
+        return Math.log(num);
+    }
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        double A,B;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Choose Operation : ");
+            System.out.print("1. Factorial\n2. Square Root\n3. Power\n4. Natural Logarithm\n5. Exit\nEnter Choice: ");
+            int ch;
+            ch = scanner.nextInt();
+            switch (ch) {
+                case 1:
+                    System.out.print("Enter the number : ");
+                    A = scanner.nextDouble();
+                    System.out.println("Factorial : "+factorial(A));
+                    System.out.println("\n");
+                    break;
+                case 2:
+                    System.out.print("Enter the number : ");
+                    A = scanner.nextDouble();
+                    System.out.println("Square Root : "+squareRoot(A));
+                    System.out.println("\n");
+                    break;
+                case 3:
+                    System.out.print("Enter the first number : ");
+                    A = scanner.nextDouble();
+                    System.out.print("Enter the second number : ");
+                    B = scanner.nextDouble();
+                    System.out.println("Power : "+power(A,B));
+                    System.out.println("\n");
+                    break;
+                case 4:
+                    System.out.print("Enter the number : ");
+                    A = scanner.nextDouble();
+                    System.out.println("Natural Log : "+Math.log(A));
+                    System.out.println("\n");
+                    break;
+                case 5: return;
+                default:
+                    System.out.println("Invalid Choice !");
+                    System.out.println("\n");
+                    break;
+            }
+        } while(true);
+    }
 }
